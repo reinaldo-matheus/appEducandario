2c20f82cf4bf5682bdcd0105c58b816931105b4b
reinaldo-matheus <reinaldomatheus011@gmail.com>
2024-07-19 16:36:23 -0300

    creat project

---
+++ src/main/java/com/appeducandario/educandario/EducandarioApplication.java
@@ -0,0 +1,13 @@
+package com.appeducandario.educandario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducandarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducandarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appeducandario/educandario/EducandarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appeducandario.educandario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducandarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
